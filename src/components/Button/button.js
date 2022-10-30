fb842e2b28e3dd9f54d1ffcdd50199c088fe6bd5
altjoe <meyer.joer5@gmail.com>
2022-10-30 19:24:21 -0400

    remove proptypes

--- src/components/Button/button.js
+++ src/components/Button/button.js
@@ -1,17 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
-import PropTypes from 'prop-types'
-
 
 export default function Button(props) {
     return (
         <div class={props.class}>{props.label}</div>
     );
-}
-Button.propTypes = {
-    class : PropTypes.string,
-    label : PropTypes.string
-}
-Button.defaultProps = {
-    class : "",
-    label: "Button"
 }
